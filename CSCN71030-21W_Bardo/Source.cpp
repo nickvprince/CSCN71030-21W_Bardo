98b0d4641baa4ca7986a121bf9701ce69f43c41a
techee12 <70961125+techee12@users.noreply.github.com>
2021-03-17 15:51:02 -0400

    main selection branch completed

--- CSCN71030-21W_Bardo/Source.cpp
+++ CSCN71030-21W_Bardo/Source.cpp
@@ -1,3 +1,26 @@
+/*
+* File Name : Main.cpp
+*
+* Programmer : Danny Smith
+* Class : Team Based Software Development CSCN71030-21W-SEC1-PROJECT 2
+* Date : February 6, 2021
+* Teacher : Dr. Elliott Coleshill
+* Team : Dominic Pham, Thomas Horvath, Nicholas, Prince, Ali Syed
+*
+* Description : Main.cpp used to test the functionality of file IO
+*
+*
+* Version : 1.0
+*
+*
+* History :
+*	ReadMe File Created, IronSword weapon test file created
+*
+* Revision Ideas -
+*	FileIO as threads to do the workload while other operations are running
+*/
+
+
 
 using namespace std;
 #include <stdlib.h>
@@ -14,38 +37,39 @@ int main() {
 
 
     // <- INIT
+
+
     while (true) {
-        //system("cls");
+        system("cls");
         printOptions();
         choice = getc(stdin);
         switch (choice) {
-        case 48: // = int 0
+        case 49: // 1
             break;
-        case 49: // = int 1
+        case 50: // 2
+            break;
+        case 51: // 3
+            break;
+        case 52: // 4
+            break;
+        case 53: // 5
             exit(0);
-        case 50:
-            // test
             break;
-        case 51:
-            //test 2
-            break;
-        case 52:
-            //test 3
-            break;
-		case 53:
-			//test 4
-			break;
         default:
-            cout << choice;
             break;
 
         }
+        while ((getchar()) != '\n');
     }
 }
 void printOptions() {
     cout << "Welcome to the Bardo home town please choose an option\n";
     cout << "-------------------------------------------------------\n";
     cout << endl;
-    cout << "1. Exit\n";
+    cout << "1. Battle\n";
+    cout << "2. Shop\n";
+    cout << "3. Forge\n";
+    cout << "4. Skill tree\n";
+    cout << "5. Exit\n";
     cout << "Enter : ";
 }
