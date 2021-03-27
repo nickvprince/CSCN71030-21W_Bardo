ecda1902762a87dd71324d64dc0fdd1d7431255c
DPham <dom17pham@gmail.com>
2021-03-26 22:32:20 -0400

    Created Crafting.cpp file Created folders to organize test files

---
+++ CSCN71030-21W_Bardo/Crafting.cpp
@@ -0,0 +1,16 @@
+// File Name : Crafting.cpp
+//
+// Programmer : Dominic Pham
+// Class : Team Based Software Development CSCN71030-21W-SEC1-PROJECT 2
+// Date : March 26, 2021
+// Teacher : Dr. Elliott Coleshill
+// Team : Danny Smith, Thomas Horvath, Nicholas Prince, Ali Syed
+//
+// Description: Tests crafting functions such as creating and managing items in inventory
+//
+//
+// Version : 1.0
+
+int Crafting(void) {
+
+}
