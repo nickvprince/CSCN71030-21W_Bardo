b5d5cd637b4b52f8ac01155c57938d017ecc9c2f
DPham <dom17pham@gmail.com>
2021-04-06 17:44:08 -0400

    minor bug fix in search function

--- CSCN71030-21W_Bardo/By_Search.cpp
+++ CSCN71030-21W_Bardo/By_Search.cpp
@@ -13,42 +13,43 @@ void By_Search(inventory i) {
 		cout << "Enter the item name to search (type <exit> to quit): "; 
 		getline(cin, find_item);
 		system("cls");
-
-		if (find_item != "exit") {
-			for (int a = 0; a < MAX_MATERIAL; a++) {
-				if (find_item == i.material[a].getItemName() && i.material[a].getQuantity() != 0) {
-					cout << "Name: " << i.material[a].getItemName() << "\t\t\t\t Quantity: " << i.material[a].getQuantity() << endl;
-					cout << "Description: " << i.material[a].getItemDescription() << endl << endl;
-					found_item = true;
-					break;
-				}
-			}
-			if (found_item == false) {
-				for (int a = 0; a < MAX_WEAPON; a++) {
-					if (find_item == i.weapon[a].getItemName() && i.weapon[a].getQuantity() != 0) {
-						cout << "Name: " << i.weapon[a].getItemName() << "\t\t\t\t Quantity: " << i.weapon[a].getQuantity() << endl;
-						cout << "Description: " << i.weapon[a].getItemDescription() << endl << endl;
+		if (find_item != "") {
+			if (find_item != "exit") {
+				for (int a = 0; a < MAX_MATERIAL; a++) {
+					if (find_item == i.material[a].getItemName() && i.material[a].getQuantity() != 0) {
+						cout << "Name: " << i.material[a].getItemName() << "\t\t\t\t Quantity: " << i.material[a].getQuantity() << endl;
+						cout << "Description: " << i.material[a].getItemDescription() << endl << endl;
 						found_item = true;
 						break;
 					}
 				}
-			}
-			if (found_item == false) {
-				for (int a = 0; a < MAX_SPELL; a++) {
-					if (find_item == i.spell[a].getItemName() && i.spell[a].getQuantity() != 0) {
-						cout << "Name: " << i.spell[a].getItemName() << "\t\t\t\t Quantity: " << i.spell[a].getQuantity() << endl;
-						cout << "Description: " << i.spell[a].getItemDescription() << endl << endl;
-						found_item = true;
-						break;
+				if (found_item == false) {
+					for (int a = 0; a < MAX_WEAPON; a++) {
+						if (find_item == i.weapon[a].getItemName() && i.weapon[a].getQuantity() != 0) {
+							cout << "Name: " << i.weapon[a].getItemName() << "\t\t\t\t Quantity: " << i.weapon[a].getQuantity() << endl;
+							cout << "Description: " << i.weapon[a].getItemDescription() << endl << endl;
+							found_item = true;
+							break;
+						}
 					}
 				}
+				if (found_item == false) {
+					for (int a = 0; a < MAX_SPELL; a++) {
+						if (find_item == i.spell[a].getItemName() && i.spell[a].getQuantity() != 0) {
+							cout << "Name: " << i.spell[a].getItemName() << "\t\t\t\t Quantity: " << i.spell[a].getQuantity() << endl;
+							cout << "Description: " << i.spell[a].getItemDescription() << endl << endl;
+							found_item = true;
+							break;
+						}
+					}
+				}
+				if (found_item == false) {
+					cout << "Item not found in inventory." << endl << endl;
+				}
 			}
-			if (found_item == false) {
-				cout << "Item not found in inventory." << endl << endl;
+			else {
+				return;
 			}
-		}
-		else {
-			return;
 		}
 	}
 }
